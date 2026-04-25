b4bc78d7282257e84fe076f1b5de1d905b4cac73
vithorrosa <vithorrosa@outlook.com>
2026-04-25 14:05:55 -0300

    test: adiciona automacao de campos obrigatorios no login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,26 +1,26 @@
 describe('Login - SauceDemo', () => {
-  it('CT-001 - Deve realizar login com credenciais válidas', () => {
-    cy.visit('https://www.saucedemo.com/')
+    it('CT-001 - Deve realizar login com credenciais válidas', () => {
+        cy.visit('https://www.saucedemo.com/')
 
-    cy.get('[data-test="username"]').type('standard_user')
-    cy.get('[data-test="password"]').type('secret_sauce')
-    cy.get('[data-test="login-button"]').click()
+        cy.get('[data-test="username"]').type('standard_user')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('[data-test="login-button"]').click()
 
-    cy.url().should('include', '/inventory.html')
-    cy.get('[data-test="title"]').should('contain', 'Products')
-  })
+        cy.url().should('include', '/inventory.html')
+        cy.get('[data-test="title"]').should('contain', 'Products')
+    })
 
-  it('CT-002 - Deve bloquear login com usuário bloqueado', () => {
-    cy.visit('https://www.saucedemo.com/')
+    it('CT-002 - Deve bloquear login com usuário bloqueado', () => {
+        cy.visit('https://www.saucedemo.com/')
 
-    cy.get('[data-test="username"]').type('locked_out_user')
-    cy.get('[data-test="password"]').type('secret_sauce')
-    cy.get('[data-test="login-button"]').click()
+        cy.get('[data-test="username"]').type('locked_out_user')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('[data-test="login-button"]').click()
 
-    cy.get('[data-test="error"]')
-      .should('be.visible')
-      .and('contain', 'Sorry, this user has been locked out.')
-  })
+        cy.get('[data-test="error"]')
+            .should('be.visible')
+            .and('contain', 'Sorry, this user has been locked out.')
+    })
 })
 it('CT-003 - Deve exibir erro ao tentar login sem preencher usuário', () => {
   cy.visit('https://www.saucedemo.com/')
@@ -30,5 +30,15 @@ it('CT-003 - Deve exibir erro ao tentar login sem preencher usuário', () => {
 
   cy.get('[data-test="error"]')
     .should('be.visible')
-    .and('contain', 'Username is required')
+    .and('contain.text', 'Username is required')
+})
+it('CT-004 - Deve exibir erro ao tentar login sem preencher senha', () => {
+  cy.visit('https://www.saucedemo.com/')
+
+  cy.get('[data-test="username"]').type('standard_user')
+  cy.get('[data-test="login-button"]').click()
+
+  cy.get('[data-test="error"]')
+    .should('be.visible')
+    .and('contain.text', 'Password is required')
 })

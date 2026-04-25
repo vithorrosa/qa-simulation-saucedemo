85beb269a3881a9b81fca4690d6084d745d944cf
vithorrosa <vithorrosa@outlook.com>
2026-04-25 13:45:53 -0300

    test: adiciona automacao de login com usuario bloqueado

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -9,4 +9,16 @@ describe('Login - SauceDemo', () => {
     cy.url().should('include', '/inventory.html')
     cy.get('[data-test="title"]').should('contain', 'Products')
   })
+
+  it('CT-002 - Deve bloquear login com usuário bloqueado', () => {
+    cy.visit('https://www.saucedemo.com/')
+
+    cy.get('[data-test="username"]').type('locked_out_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+
+    cy.get('[data-test="error"]')
+      .should('be.visible')
+      .and('contain', 'Sorry, this user has been locked out.')
+  })
 })

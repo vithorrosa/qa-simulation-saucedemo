1acf4b0b1f3450cfd734223d763a94b2d4efb718
vithorrosa <vithorrosa@outlook.com>
2026-04-25 13:06:52 -0300

    test: adiciona automacao inicial de login com Cypress

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,12 @@
+describe('Login - SauceDemo', () => {
+  it('CT-001 - Deve realizar login com credenciais válidas', () => {
+    cy.visit('https://www.saucedemo.com/')
+
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+
+    cy.url().should('include', '/inventory.html')
+    cy.get('[data-test="title"]').should('contain', 'Products')
+  })
+})
